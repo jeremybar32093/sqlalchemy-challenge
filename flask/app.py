0bc78698f35ab11b37da77278872c89d2a8966a5
Jeremy Bar <jeremybar@live.com>
2021-05-19 19:39:15 -0400

    added working folder for testing api results in jupyter notebook, added tobs route to flask api

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,6 @@
 import numpy as np
+import datetime as dt
+from dateutil.relativedelta import relativedelta
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -48,7 +50,7 @@ def stations():
     session = Session(engine)
 
     """Return a list of all stations"""
-    # Query all passengers
+    # Query all stations
     results = session.query(Station.station,Station.name).all()
 
     session.close()
@@ -62,6 +64,43 @@ def stations():
         all_stations.append(station_dict)
 
     return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all dates/temperatures for the most active station in the last year"""
+    # First, get latest measurement taken and convert to datetime object
+    max_date = session.query(func.max(Measurement.date)).all()[0][0]
+    max_date_datetime = dt.datetime.strptime(max_date, '%Y-%m-%d')
+    # Calculate date as of 1 year ago - these 2 dates will constitute date range for filtering
+    max_date_minus_1yr = max_date_datetime - relativedelta(years=1)
+
+    # Calculate most active station (station with highest count of measurements)
+    most_active_station = session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).\
+                          order_by(func.count(Measurement.station).desc()).first()[0]
+
+
+    # Return the last year's temperatures for the most active station calculated above
+    results = session.query(Measurement.station,Measurement.date,Measurement.tobs).\
+              filter(Measurement.station == most_active_station).\
+              filter(Measurement.date >= max_date_minus_1yr).\
+              filter(Measurement.date <= max_date).\
+              all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_stations
+    most_active_station_temps = []
+    for station, date, tobs in results:
+        temp_dict = {}
+        temp_dict["station"] = station
+        temp_dict["date"] = date
+        temp_dict["tobs"] = tobs
+        most_active_station_temps.append(temp_dict)
+
+    return jsonify(most_active_station_temps)
 
 
 #################################################
